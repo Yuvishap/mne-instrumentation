f519c0810fa5e619d5c916a059059af9c1dba4b2
Yuval Shapira <yuvishap@gmail.com>
2025-05-23 18:42:55 +0300

    Support local file sharing into docker image

--- mne-server/main.py
+++ mne-server/main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -14,4 +16,16 @@ app.add_middleware(
 
 @app.get("/api/ping")
 def ping():
+    file_path = SHARED_DIR / "test_write"
+
+    # Create an empty file
+    file_path.touch()
     return {"message": "pong"}
+
+server_shared_path = os.environ.get("SERVER_SHARED_DATA_PATH", "/app/shared")
+local_shared_path = os.environ.get("LOCAL_SHARED_DATA_PATH", "/shared_files")
+SHARED_DIR = Path(server_shared_path)
+
+@app.get("/files")
+def list_files():
+    return [(f.name, str(f).replace(server_shared_path, local_shared_path)) for f in SHARED_DIR.glob("*") if f.is_file()]
